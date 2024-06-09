f786ff3d482254e5256a6e1e8fe7e194ed759bd4
Prabhjot Singh Rai <hi@psr.ai>
2024-06-09 16:55:42 -0500

    chore: use cpu explicitly for lightning module as accelerator

--- multitask_classifier_lightening.py
+++ multitask_classifier_lightening.py
@@ -255,7 +255,7 @@ def train_multitask(args):
 
     lightening_params = {
         'precision': args.precision,
-        'accelerator': 'auto' if args.use_gpu else None,
+        'accelerator': 'auto' if args.use_gpu else 'cpu',
         'devices': 'auto'
     }
 
